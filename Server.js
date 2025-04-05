21a315ffaaa977e8c00cf22ef19a32d5496f50b0
Your Name <ervthirumal14321@gmail.com>
2025-04-05 21:01:58 +0530

    update cors

--- Server.js
+++ Server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://portfolio-frontend-92nm.onrender.com',
 }));
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
