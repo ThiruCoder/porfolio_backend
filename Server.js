dacb5083832cf7d4a22421254b44ab0a5c6021bc
Your Name <ervthirumal14321@gmail.com>
2025-04-12 07:27:59 +0530

    serverChange

--- Server.js
+++ Server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use(cors());
 app.use(cors({
-    origin: backendUrl, // Frontend URL
+    origin: backendUrl || backendTrilUrl // Frontend URL
 }));
 
 
